6857067ee97869decc355b2d218d2cd5aafcb603
Rahul Bhilesha <bhilesharahul@gmail.com>
2021-08-05 16:46:38 +0530

    Learn and implemented basic routing and controller to print the lucky number.

---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class LuckyController extends Controller
+{
+    /**
+     * @Route("/lucky/number/{max}")
+     */
+    public function numberAction($max)
+    {
+        $number = random_int(0, $max);
+
+        return new Response(
+            '<html><body>Lucky number: ' . $number . '</body></html>'
+        );
+    }
+}

174b5532a4df1f80b6b146ed8e3215f72bf8f0b6
Rahul Bhilesha <bhilesharahul@gmail.com>
2021-08-07 23:47:09 +0530

    Implemented logic to add todos.

--- src/AppBundle/Controller/TodoController.php
+++ src/AppBundle/Controller/TodoController.php
@@ -3,9 +3,11 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Todo;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
@@ -32,18 +34,47 @@ class TodoController extends Controller
     public function createAction(Request $request)
     {
         $todo = new Todo;
-
+        // To create form to add todo
         $form = $this->createFormBuilder($todo)
             ->add('name', TextType::class, ['attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']])
             ->add('category', TextType::class, ['attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']])
             ->add('description', TextareaType::class, ['attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']])
             ->add('priority', ChoiceType::class, ['choices' => ['Low' => 'Low', 'Normal' => 'Normal', 'High' => 'High'], 'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']])
-            ->add('due_date', DateTimeType::class, ['attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']])
+            ->add('due_date', DateTimeType::class, ['attr' => ['class' => 'form_control', 'style' => 'margin-bottom:15px']])
+            ->add('save', SubmitType::class, ['label' => 'Create Todo', 'attr' => ['class' => 'btn btn-primary', 'style' => 'margin-bottom:15px']])
             ->getForm();
 
+        $form->handleRequest($request);
+
+        // if form is submitted and valid
         if ($form->isSubmitted() && $form->isValid()) {
-            die('submitted');
+            //Get Data
+            $name = $form['name']->getData();
+            $category = $form['category']->getData();
+            $description = $form['description']->getData();
+            $priority = $form['priority']->getData();
+            $due_date = $form['due_date']->getData();
+            $now = new DateTime('now');
+
+            // set variables
+            $todo->setName($name);
+            $todo->setCategory($category);
+            $todo->setDescription($description);
+            $todo->setPriority($priority);
+            $todo->setDueDate($due_date);
+            $todo->setCreateDate($now);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($todo);
+            $em->flush();
+
+            $this->addFlash(
+                'notice',
+                'Todo Added.'
+            );
+            return $this->redirectToRoute('todo_list');
         }
+
         return $this->render('todo/create.html.twig', [
             'form' => $form->createView(),
         ]);
